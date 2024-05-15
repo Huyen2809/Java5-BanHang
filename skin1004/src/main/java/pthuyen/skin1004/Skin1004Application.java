6b7c6cfb0384f2dce0f186cf78a5629bab86b313
Phạm Thị Thuý Huyền <148331300+Huyen2809@users.noreply.github.com>
2024-05-15 17:48:37 +0700

    Add files via upload

---
+++ skin1004/src/main/java/pthuyen/skin1004/Skin1004Application.java
@@ -0,0 +1,13 @@
+package pthuyen.skin1004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skin1004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skin1004Application.class, args);
+	}
+
+}
---
+++ skin1004/src/test/java/pthuyen/skin1004/Skin1004ApplicationTests.java
@@ -0,0 +1,13 @@
+package pthuyen.skin1004;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skin1004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
